9a2bd80587317431cd3cbdb08fccfa0019ad1f0d
terrencetec <terrencetec@gmail.com>
2022-05-08 21:57:21 +0800

    qtile config bugfix

--- dotfiles/.config/qtile/mouse.py
+++ dotfiles/.config/qtile/mouse.py
@@ -9,7 +9,7 @@ import os
 from libqtile.lazy import lazy
 from libqtile.config import Click, Drag
 
-from _keys import decipher_shortcut, mod
+from keys import decipher_shortcut, mod
 
 
 path_home = os.path.expanduser('~')
